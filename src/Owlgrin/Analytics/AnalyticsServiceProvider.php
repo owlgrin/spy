53eefa5edaf19e145a682aa66cdaa9757bff998a
Sahil <sahilsarpal@gmail.com>
2015-10-06 16:22:39 +0530

    first commit

---
+++ src/Owlgrin/Analytics/AnalyticsServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Owlgrin\Analytics;
+
+use Illuminate\Support\ServiceProvider;
+
+class AnalyticsServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
